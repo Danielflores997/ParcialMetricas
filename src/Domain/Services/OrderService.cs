711250e5e2059067d12c4e9dd6de69db05bac9e9
Danielflores997 <daniflores5492@gmail.com>
2025-11-17 23:31:02 -0500

    cambio visibilidad

--- src/Domain/Services/OrderService.cs
+++ src/Domain/Services/OrderService.cs
@@ -8,12 +8,14 @@ using System.Collections.Immutable;
 
 public static class OrderService
 {
-    public static ImmutableList<Order> LastOrders = ImmutableList<Order>.Empty;
+    private static ImmutableList<Order> _lastOrders = ImmutableList<Order>.Empty;
+
+    public static IReadOnlyList<Order> LastOrders => _lastOrders;
 
     public static Order CreateTerribleOrder(string customer, string product, int qty, decimal price)
     {
         var o = new Order { Id = new Random().Next(1, 9999999), CustomerName = customer, ProductName = product, Quantity = qty, UnitPrice = price };
-        LastOrders = LastOrders.Add(o);
+        _lastOrders = _lastOrders.Add(o);
         Infrastructure.Logging.Logger.Log("Created order " + o.Id + " for " + customer);
         return o;
     }
