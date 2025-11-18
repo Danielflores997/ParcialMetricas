0ebc1b24d2e50edfc7356f09008bd2404b53a184
Danielflores997 <daniflores5492@gmail.com>
2025-11-17 20:51:58 -0500

    Agregar archivos de proyecto.

---
+++ src/Infrastructure/Data/BadDb.cs
@@ -0,0 +1,27 @@
+using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace Infrastructure.Data;
+
+public static class BadDb
+{
+    public static string ConnectionString = "Server=localhost;Database=master;User Id=sa;Password=SuperSecret123!;TrustServerCertificate=True";
+
+
+    public static int ExecuteNonQueryUnsafe(string sql)
+    {
+        var conn = new SqlConnection(ConnectionString);
+        var cmd = new SqlCommand(sql, conn);
+        conn.Open();
+        return cmd.ExecuteNonQuery();
+    }
+
+    public static IDataReader ExecuteReaderUnsafe(string sql)
+    {
+        var conn = new SqlConnection(ConnectionString);
+        var cmd = new SqlCommand(sql, conn);
+        conn.Open();
+        return cmd.ExecuteReader();
+    }
+}
