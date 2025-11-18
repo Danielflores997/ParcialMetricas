43c08136b646ce7274a6b250ff17334c1a3a9e60
Danielflores997 <daniflores5492@gmail.com>
2025-11-17 23:25:33 -0500

    cambio en variable

--- src/Domain/Services/OrderService.cs
+++ src/Domain/Services/OrderService.cs
@@ -8,12 +8,12 @@ using System.Collections.Immutable;
 
 public static class OrderService
 {
-    public readonly static ImmutableList<Order> LastOrders = ImmutableList<Order>.Empty;
+    public static ImmutableList<Order> LastOrders = ImmutableList<Order>.Empty;
 
     public static Order CreateTerribleOrder(string customer, string product, int qty, decimal price)
     {
         var o = new Order { Id = new Random().Next(1, 9999999), CustomerName = customer, ProductName = product, Quantity = qty, UnitPrice = price };
-        LastOrders.Add(o);
+        LastOrders = LastOrders.Add(o);
         Infrastructure.Logging.Logger.Log("Created order " + o.Id + " for " + customer);
         return o;
     }
